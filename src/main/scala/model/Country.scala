127245b30b6b51015d730f897d431713a1556f9d
Pomaranch <allana437@gmail.com>
2017-11-12 15:39:25 +0200

    model details

--- src/main/scala/model/Country.scala
+++ src/main/scala/model/Country.scala
@@ -1,5 +1,14 @@
 package model
 
-case class Country(id: Option[Long], title: String) {
+import slick.lifted.Tag
+import slick.jdbc.PostgresProfile.api._
 
+
+case class Country(id: Option[Long], title: String)
+
+class CountryTable(tag: Tag) extends Table[Country](tag, "countries") {
+  val id = column[Option[Long]]("id", O.PrimaryKey)
+  val title = column[String]("title")
+
+  def * = (id, title) <> (Country.apply _ tupled, Country.unapply)
 }
